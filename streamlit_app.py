07a85ab6f420cca521e4353f0867eddf6224c273
Aseuc <78257976+Aseuc@users.noreply.github.com>
2023-04-25 10:54:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,21 @@
 import streamlit as st
+upload()
+
+
+
 
 if st.button('Start Analyse'):
     st.write('Why hello there') 
     
-#def upload():
- #   uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-  #  for uploaded_file in uploaded_files:
+def upload():
+    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
         #bytes_data = uploaded_file.read()
         #st.write("filename:", uploaded_file.name)
         #st.write(bytes_data)
 
         
-#upload()
+
 
 
 
