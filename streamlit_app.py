2beb889229be4b34faedf25bebb7e281cdd909a0
Aseuc <78257976+Aseuc@users.noreply.github.com>
2023-04-25 21:51:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,27 +9,27 @@ def upload():
 
 upload()
 
-    
-# data = pd.read_excel('C:/Users/busse/OneDrive/Desktop/rennengehenstreamlit/streamlit-main/TestData.xlsx')
-# np.random.seed(42)
-# X = data.drop('target', axis=1)
-# y = data["target"]
-# X_train, X_test, y_train, y_test = sk.model_selection.train_test_split(X, y, test_size=0.2)
-# clf = RandomForestClassifier(n_estimators=100)
-# clf.fit(X_train, y_train)
-# X_data = pd.read_csv('newTest.csv')
-# clf.predict(X_data)
-# #X_test.head()
-# prob = clf.predict_proba(X_data[:100])
-# #print(prob)
-# X_data["prediction_prob"] = clf.predict(X_data)
-# pd.set_option('display.max_rows', None)
-        
+def  dataAnalyze():
+        data = pd.read_excel('TestData.xlsx')
+        np.random.seed(42)
+        X = data.drop('target', axis=1)
+        y = data["target"]
+        X_train, X_test, y_train, y_test = sk.model_selection.train_test_split(X, y, test_size=0.2)
+        clf = RandomForestClassifier(n_estimators=100)
+        clf.fit(X_train, y_train)
+        X_data = pd.read_csv('newTest.csv')
+        clf.predict(X_data)
+        # #X_test.head()
+        prob = clf.predict_proba(X_data[:100])
+        # #print(prob)
+        X_data["prediction_prob"] = clf.predict(X_data)
+        pd.set_option('display.max_rows', None)
+        X_data.head()        
       
        
        
 if st.button('Start Analyse'):
-    st.write('Why hello there') 
+    dataAnalyze() 
         
 
 
